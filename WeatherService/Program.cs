944274866dcf266ca42e721116be138161d96cef
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 20:47:21 +0300

    Add Program entry point and ShowMainMenu for user interaction

--- WeatherService/Program.cs
+++ WeatherService/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WeatherService;
+using WeatherService.Services;
+using WeatherService.Utilities;
+
+class Program
+{
+    static void Main()
+    {
+        var configFilePath = @"C:\Users\maham\Desktop\WeatherService\WeatherService\botconfig.json";
+        var botConfigs = BotConfigurationLoader.LoadFromFile(configFilePath);
+        var botFactory = new WeatherBotFactory(botConfigs);
+        var bots = botConfigs.Keys.Where(botType => botConfigs[botType].Enabled) .Select(botFactory.CreateBot).ToList();
+
+        var weatherService = new WeatherServices(bots);
+        var jsonConverter = new JSONWeatherDataConverter();
+        var xmlConverter = new XMLWeatherDataConverter();
+        var menu = new ShowMainMenu(weatherService, jsonConverter, xmlConverter);
+        menu.ShowMenu();
+    }
+}
