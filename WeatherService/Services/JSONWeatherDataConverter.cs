8ae822d17d1dbb3f8ed5455877a492c791eae703
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 17:11:24 +0300

    Added JSONWeatherDataConverter class implementing IWeatherDataConverter

---
+++ WeatherService/Services/JSONWeatherDataConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using WeatherService.Interfaces;
+using WeatherService.Models;
+
+namespace WeatherService.Services
+{
+    public class JSONWeatherDataConverter : IWeatherDataConverter
+    {
+        public WeatherData Convert(string inputData) => JsonSerializer.Deserialize<WeatherData>(inputData);
+    }
+}
