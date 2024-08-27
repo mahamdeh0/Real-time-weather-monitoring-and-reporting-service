02a0a1d353ad78b04c08be07498f1952f0e18007
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 17:13:53 +0300

    Added XMLWeatherDataConverter class implementing IWeatherDataConverter

---
+++ WeatherService/Services/XMLWeatherDataConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using WeatherService.Interfaces;
+using WeatherService.Models;
+
+namespace WeatherService.Services
+{
+    public class XMLWeatherDataConverter : IWeatherDataConverter
+    {
+        public WeatherData Convert(string inputData)
+        {
+            var serializer = new XmlSerializer(typeof(WeatherData));
+            using (var reader = new StringReader(inputData))
+            {
+                return (WeatherData)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
