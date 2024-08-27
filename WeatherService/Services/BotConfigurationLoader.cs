8661480a0a5492eaead4b8208b5ea18b450540f9
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 20:33:40 +0300

    Add BotConfigurationLoader to load bot configurations from JSON file

---
+++ WeatherService/Services/BotConfigurationLoader.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using WeatherService.Models;
+using WeatherService.Models.Enums;
+
+namespace WeatherService
+{
+    public static class BotConfigurationLoader
+    {
+        public static Dictionary<weatherbots, BotConfiguration> LoadFromFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            var Configs = JsonConvert.DeserializeObject<Dictionary<string, BotConfiguration>>(json);
+
+            var result = new Dictionary<weatherbots, BotConfiguration>();
+
+            foreach (var x in Configs)
+            {
+                if (Enum.TryParse(x.Key, out weatherbots botType))
+                {
+                    result[botType] = x.Value;
+                }
+            }
+
+            return result;
+        }
+    }
+}
