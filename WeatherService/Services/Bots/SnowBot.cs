885aef77b5d6ad57d4273eadca372d172ca3777e
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 17:55:33 +0300

    Add RainBot, SunBot, SnowBot classes with activation thresholds and messages

---
+++ WeatherService/Services/Bots/SnowBot.cs
@@ -0,0 +1,26 @@
+﻿using WeatherService.Interfaces;
+using WeatherService.Models;
+
+namespace WeatherService.Services.Bots
+{
+    public class SnowBot : IWeatherBot
+    {
+        private readonly double _threshold;
+        private readonly string _message;
+
+        public SnowBot(double Threshold, string Message)
+        {
+            _threshold = Threshold;
+            _message = Message;
+        }
+
+        public void Activate(WeatherData data)
+        {
+            if (data.Temperature < _threshold)
+            {
+                Console.WriteLine("SnowBot activated");
+                Console.WriteLine(_message);
+            }
+        }
+    }
+}
---
+++ WeatherService/Services/Bots/SunBot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherService.Interfaces;
+using WeatherService.Models;
+
+namespace WeatherService.Services.Bots
+{
+    public class SunBot : IWeatherBot
+    {
+        private readonly double _threshold;
+        private readonly string _message;
+
+        public SunBot(double Threshold, string Message)
+        {
+            _threshold = Threshold;
+            _message = Message;
+        }
+
+        public void Activate(WeatherData data)
+        {
+            if (data.Temperature > _threshold)
+            {
+                Console.WriteLine("SunBot activated");
+                Console.WriteLine(_message);
+            }
+        }
+    }
+}
