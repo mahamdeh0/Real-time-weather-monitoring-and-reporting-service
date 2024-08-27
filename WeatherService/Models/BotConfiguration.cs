265ae2a66b03a29f2af1ec9c1d5aaa5428949690
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 16:31:00 +0300

    Added BotConfiguration class with Enabled, Threshold, Message properties

---
+++ WeatherService/Models/BotConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherService.Models
+{
+    public class BotConfiguration
+    {
+        public bool Enabled { get; set; }
+        public double Threshold { get; set; }
+        public string Message { get; set; }
+    }
+}
