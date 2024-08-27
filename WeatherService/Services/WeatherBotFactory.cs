2c3c442a5b7d29f2b3ea9b839cd438beba0b26c8
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-27 18:03:32 +0300

    Added WeatherBotFactory class for creating configured bot

---
+++ WeatherService/Services/WeatherBotFactory.cs
@@ -0,0 +1,33 @@
+﻿using WeatherService.Interfaces;
+using WeatherService.Models.Enums;
+using WeatherService.Models;
+using WeatherService.Services.Bots;
+
+namespace WeatherService.Services
+{
+    public class WeatherBotFactory : IWeatherBotFactory
+    {
+        private readonly BotConfiguration _config;
+
+        public WeatherBotFactory(BotConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IWeatherBot CreateBot(weatherbots botType)
+        {
+            switch (botType)
+            {
+                case weatherbots.RainBot:
+                    return new RainBot(_config.Threshold, _config.Message);
+                case weatherbots.SunBot:
+                    return new SunBot(_config.Threshold, _config.Message);
+                case weatherbots.SnowBot:
+                    return new SnowBot(_config.Threshold, _config.Message);
+                default:
+                    throw new ArgumentException("Invalid bot type");
+            }
+        }
+    }
+
+}
